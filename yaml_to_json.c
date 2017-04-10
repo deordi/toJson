1db9aca3e2c25b2eb849019aae366d438666e6db
Georgina <georgina@GH-laptop.local>
2017-04-10 16:46:40 +0100

    Committer: Georgina <georgina@GH-laptop.local>
    Changes to be committed:
        modified:   yaml_to_json.c - indentation

--- yaml_to_json.c
+++ yaml_to_json.c
@@ -25,9 +25,9 @@ static void report_parser_error(yaml_parser_t*);
 int
 process_yaml(FILE* instream, FILE* outstream) {
 
-	assert(instream);
-	assert(outstream);
-	
+    assert(instream);
+    assert(outstream);
+    
     yaml_parser_t parser;
     yaml_event_t event;
 
@@ -35,12 +35,12 @@ process_yaml(FILE* instream, FILE* outstream) {
 
     memset(&parser, 0, sizeof(parser));
     memset(&event, 0, sizeof(event));
-	
+
     /* Initialize the parser and emitter objects. */
 
     if (!yaml_parser_initialize(&parser)) {
         fprintf(stderr, "Could not initialize the parser object\n");
-	    goto return_error;
+        goto return_error;
     }
 
     /* Set the parser parameters. */
@@ -50,8 +50,8 @@ process_yaml(FILE* instream, FILE* outstream) {
     /* Get the first event. */
 
     if (!yaml_parser_parse(&parser, &event)) {
-    	report_parser_error(&parser);
-	    goto return_error;
+        report_parser_error(&parser);
+        goto return_error;
     }
 
     /* Check if this is the stream end. */
@@ -60,38 +60,38 @@ process_yaml(FILE* instream, FILE* outstream) {
         fprintf(stderr, "Event error: wrong type of event: %d\n", event.type);
     }
 
-	if (!process_yaml_stream(&parser, &event, outstream)) {
-		fprintf(stderr, "Stream error: error processing stream\n");
-		goto return_error;
-	}
-	
+    if (!process_yaml_stream(&parser, &event, outstream)) {
+        fprintf(stderr, "Stream error: error processing stream\n");
+        goto return_error;
+    }
+    
     /* Delete the event object and the parser. */
 
     yaml_event_delete(&event);
-	yaml_parser_delete(&parser);
-	
-	return 1;
-	
+    yaml_parser_delete(&parser);
+    
+    return 1;
+
 return_error:
 
-	yaml_event_delete(&event);
-	yaml_parser_delete(&parser);
-	
-	return 0;
+    yaml_event_delete(&event);
+    yaml_parser_delete(&parser);
+
+    return 0;
 
 }
 
 int
 process_yaml_stream(yaml_parser_t* parser, yaml_event_t* event, FILE* outstream) {
-	
-	assert(parser);
-	assert(event);
-	assert(outstream);
-	
-	int done = 0;
+
+    assert(parser);
+    assert(event);
+    assert(outstream);
+
+    int done = 0;
 
     //fprintf(stdout, "Start of translated output\n");
-	
+
     while (!done)
     {
         /* Get the next event. */
@@ -108,10 +108,10 @@ process_yaml_stream(yaml_parser_t* parser, yaml_event_t* event, FILE* outstream)
             case YAML_STREAM_START_EVENT:
 
                 fprintf(stderr, "Event error: YAML_STREAM_START_EVENT. Expected YAML_DOCUMENT_START_EVENT or YAML_STREAM_END_EVENT.\n");
-				goto return_error;
+                goto return_error;
 
             case YAML_STREAM_END_EVENT:
-			
+
                 yaml_event_delete(event);
                 //fprintf(stdout, "End of translated output\n");
                 done = 1;
@@ -120,21 +120,21 @@ process_yaml_stream(yaml_parser_t* parser, yaml_event_t* event, FILE* outstream)
             case YAML_DOCUMENT_START_EVENT:
 
                 yaml_event_delete(event);
-				if (!process_yaml_document(parser, event, outstream)) {
-					fprintf(stderr, "Stream error: error processing stream\n");
-					goto return_error;   
-				}
-				break;
+                if (!process_yaml_document(parser, event, outstream)) {
+                    fprintf(stderr, "Stream error: error processing stream\n");
+                    goto return_error;   
+                }
+                break;
 
             case YAML_DOCUMENT_END_EVENT:
 
                 fprintf(stderr, "Event error: YAML_DOCUMENT_END_EVENT. Expected YAML_DOCUMENT_START_EVENT or YAML_STREAM_END_EVENT.\n");
                 goto return_error;
 
-	        case YAML_ALIAS_EVENT:
+            case YAML_ALIAS_EVENT:
 
-	            fprintf(stderr, "Event error: YAML_ALIAS_EVENT. Expected YAML_DOCUMENT_START_EVENT or YAML_STREAM_END_EVENT.\n");
-	            goto return_error;
+                fprintf(stderr, "Event error: YAML_ALIAS_EVENT. Expected YAML_DOCUMENT_START_EVENT or YAML_STREAM_END_EVENT.\n");
+                goto return_error;
 
             case YAML_SCALAR_EVENT:
 
@@ -167,29 +167,29 @@ process_yaml_stream(yaml_parser_t* parser, yaml_event_t* event, FILE* outstream)
         }
 
     }
-	
-	return 1;
-	
+
+    return 1;
+
 return_error:
-	
+
     /* Delete the event object. */
 
     yaml_event_delete(event);
-	return 0;
-	
+    return 0;
+
 }
 
 int
 process_yaml_document(yaml_parser_t* parser, yaml_event_t* event, FILE* outstream) {
-	
-	assert(parser);
-	assert(event);
-	assert(outstream);
-	
-	int done = 0;
+
+    assert(parser);
+    assert(event);
+    assert(outstream);
+
+    int done = 0;
 
     //fprintf(stdout, "Start of document output\n");
-	
+
     while (!done)
     {
         /* Get the next event. */
@@ -207,11 +207,11 @@ process_yaml_document(yaml_parser_t* parser, yaml_event_t* event, FILE* outstrea
 
                 fprintf(stderr, "Event error: YAML_STREAM_START_EVENT. Expected YAML_MAPPING_START_EVENT, \
                     YAML_SEQUENCE_START_EVENT or YAML_DOCUMENT_END_EVENT.\n");
-				goto return_error;
+                goto return_error;
 
             case YAML_STREAM_END_EVENT:
-			
-                fprintf(stderr, "Event error: YAML_STREAM_END_EVENT. Expected YAML_MAPPING_START_EVENT, \
+
+                    fprintf(stderr, "Event error: YAML_STREAM_END_EVENT. Expected YAML_MAPPING_START_EVENT, \
                     YAML_SEQUENCE_START_EVENT or YAML_DOCUMENT_END_EVENT.\n");
                 goto return_error;
 
@@ -228,7 +228,7 @@ process_yaml_document(yaml_parser_t* parser, yaml_event_t* event, FILE* outstrea
                 done = 1;
                 break;
 
-	        case YAML_ALIAS_EVENT:
+            case YAML_ALIAS_EVENT:
 
                 fprintf(stderr, "Event error: YAML_ALIAS_EVENT. Expected YAML_MAPPING_START_EVENT, \
                     YAML_SEQUENCE_START_EVENT or YAML_DOCUMENT_END_EVENT.\n");
@@ -262,7 +262,7 @@ process_yaml_document(yaml_parser_t* parser, yaml_event_t* event, FILE* outstrea
                     fprintf(stderr, "Stream error: error processing stream\n");
                     goto return_error;   
                 }
-			    break;
+                break;
 
             case YAML_MAPPING_END_EVENT:
 
@@ -276,31 +276,31 @@ process_yaml_document(yaml_parser_t* parser, yaml_event_t* event, FILE* outstrea
         }
 
     }
-	
-	return 1;
-	
+
+    return 1;
+    
 return_error:
-	
+    
     /* Delete the event object. */
 
     yaml_event_delete(event);
-	return 0;
-	
+    return 0;
+    
 }
 
 int
 process_yaml_sequence(yaml_parser_t* parser, yaml_event_t* event, FILE* outstream) {
-	
-	assert(parser);
-	assert(event);
-	assert(outstream);
-	
-	int done = 0;
+    
+    assert(parser);
+    assert(event);
+    assert(outstream);
+    
+    int done = 0;
     int elements = 0;
 
     //fprintf(stdout, "Start of sequence output\n");
     fprintf(outstream, "[ ");
-	
+    
     while (!done)
     {
         /* Get the next event. */
@@ -318,10 +318,10 @@ process_yaml_sequence(yaml_parser_t* parser, yaml_event_t* event, FILE* outstrea
 
                 fprintf(stderr, "Event error: YAML_STREAM_START_EVENT. Expected YAML_MAPPING_START_EVENT, \
                     YAML_SEQUENCE_START_EVENT, YAML_SCALAR_EVENT or YAML_SEQUENCE_END_EVENT.\n");
-				goto return_error;
+                goto return_error;
 
             case YAML_STREAM_END_EVENT:
-			
+
                 fprintf(stderr, "Event error: YAML_STREAM_END_EVENT. Expected YAML_MAPPING_START_EVENT, \
                     YAML_SEQUENCE_START_EVENT, YAML_SCALAR_EVENT or YAML_SEQUENCE_END_EVENT.\n");
                 goto return_error;
@@ -338,7 +338,7 @@ process_yaml_sequence(yaml_parser_t* parser, yaml_event_t* event, FILE* outstrea
                     YAML_SEQUENCE_START_EVENT, YAML_SCALAR_EVENT or YAML_SEQUENCE_END_EVENT.\n");
                 goto return_error;
 
-	        case YAML_ALIAS_EVENT:
+            case YAML_ALIAS_EVENT:
 
                 fprintf(stderr, "Event error: YAML_ALIAS_EVENT is not supported. Expected YAML_MAPPING_START_EVENT, \
                     YAML_SEQUENCE_START_EVENT, YAML_SCALAR_EVENT or YAML_SEQUENCE_END_EVENT.\n");
@@ -397,31 +397,31 @@ process_yaml_sequence(yaml_parser_t* parser, yaml_event_t* event, FILE* outstrea
         }
 
     }
-	
-	return 1;
-	
+
+    return 1;
+
 return_error:
-	
+    
     /* Delete the event object. */
 
     yaml_event_delete(event);
-	return 0;
-	
+    return 0;
+
 }
 
 int
 process_yaml_mapping(yaml_parser_t* parser, yaml_event_t* event, FILE* outstream) {
-	
-	assert(parser);
-	assert(event);
-	assert(outstream);
-	
-	int done = 0;
+
+    assert(parser);
+    assert(event);
+    assert(outstream);
+    
+    int done = 0;
     int members = 0;
 
     //fprintf(stdout, "Start of mapping output\n");
     fprintf(outstream, "{ ");
-	
+    
     while (!done)
     {
         /* Get the next event. */
@@ -430,32 +430,32 @@ process_yaml_mapping(yaml_parser_t* parser, yaml_event_t* event, FILE* outstream
             report_parser_error(parser);
             goto return_error;
         }
-        
+
         if (event->type == YAML_SCALAR_EVENT) {
-            
+
             if (members) fprintf(outstream, ", ");
             if (!output_key(event->data.scalar.value, event->data.scalar.length, outstream)) {
                 fprintf(stderr, "Error outputting key\n");
                 goto return_error;
             }
-            
+
             fprintf(outstream, ": ");
             yaml_event_delete(event);
             members = 1;
-            
+
         } else if (event->type == YAML_MAPPING_END_EVENT) {
-            
+
             yaml_event_delete(event);
             fprintf(outstream, " }");
             //fprintf(stdout, "End of mapping output\n");
             done = 1;
             continue;
-            
+
         } else {
-            
+
             fprintf(stderr, "Event error: Expected YAML_SCALAR_EVENT or YAML_MAPPING_END_EVENT.\n");
             goto return_error;
-            
+
         }
 
         /* Get the next event. */
@@ -464,7 +464,7 @@ process_yaml_mapping(yaml_parser_t* parser, yaml_event_t* event, FILE* outstream
             report_parser_error(parser);
             goto return_error;
         }
-        
+
         /* Analyze the event. */
 
         switch (event->type)
@@ -473,10 +473,10 @@ process_yaml_mapping(yaml_parser_t* parser, yaml_event_t* event, FILE* outstream
 
                 fprintf(stderr, "Event error: YAML_STREAM_START_EVENT. Expected YAML_MAPPING_START_EVENT, \
                     YAML_SEQUENCE_START_EVENT or YAML_SCALAR_EVENT.\n");
-				goto return_error;
+                goto return_error;
 
             case YAML_STREAM_END_EVENT:
-			
+
                 fprintf(stderr, "Event error: YAML_STREAM_END_EVENT. Expected YAML_MAPPING_START_EVENT, \
                     YAML_SEQUENCE_START_EVENT or YAML_SCALAR_EVENT.\n");
                 goto return_error;
@@ -493,7 +493,7 @@ process_yaml_mapping(yaml_parser_t* parser, yaml_event_t* event, FILE* outstream
                     YAML_SEQUENCE_START_EVENT or YAML_SCALAR_EVENT.\n");
                 goto return_error;
 
-	        case YAML_ALIAS_EVENT:
+            case YAML_ALIAS_EVENT:
 
                 fprintf(stderr, "Event error: YAML_ALIAS_EVENT is not supported. Expected YAML_MAPPING_START_EVENT, \
                     YAML_SEQUENCE_START_EVENT or YAML_SCALAR_EVENT.\n");
@@ -544,16 +544,16 @@ process_yaml_mapping(yaml_parser_t* parser, yaml_event_t* event, FILE* outstream
         }
 
     }
-	
-	return 1;
-	
+    
+    return 1;
+    
 return_error:
-	
+    
     /* Delete the event object. */
 
     yaml_event_delete(event);
-	return 0;
-	
+    return 0;
+
 }
 
 int
@@ -568,34 +568,34 @@ output_scalar(yaml_char_t* scalar, size_t length, FILE* outstream) {
     for (int k = 0; k < length; k++) {
         fprintf(outstream, "%c", scalar[k]);
     }
-    
+
     fprintf(outstream, "\"");
-        
-	return 1;
+
+    return 1;
 }
 
 int
 output_key(yaml_char_t* scalar, size_t length, FILE* outstream) {
-    
+
     assert(scalar);
     assert(outstream);
     assert(length > 0);
-    
+
     fprintf(outstream, "\"");
-    
+
     for (int k = 0; k < length; k++) {
         fprintf(outstream, "%c", scalar[k]);
     }
-    
+
     fprintf(outstream, "\"");
-        
-	return 1;
+
+    return 1;
 }
 
 void report_parser_error(yaml_parser_t* parser) {
-	
-	assert(parser);
-	
+
+    assert(parser);
+
     /* Display a parser error message. */
 
     switch (parser->error)
@@ -650,7 +650,7 @@ void report_parser_error(yaml_parser_t* parser) {
             fprintf(stderr, "Internal error\n");
             break;
     }
-	return;
+    return;
 }
 
 int
